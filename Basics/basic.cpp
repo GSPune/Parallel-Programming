20793beb19ba2d4114714dd4d955be97b247d6e2
Stephen George <stephengeorgepune@gmail.com>
2024-09-18 18:15:11 +0530

    Testing working of MPI

--- Basics/basic.cpp
+++ Basics/basic.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(int argc,char** argv)
 {
+	cout << "Outside MPI Env 1 and rank: unknown?" << endl;
 	int rank,size;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
@@ -11,6 +12,6 @@ int main(int argc,char** argv)
 	//cout << "Hello World" << endl;
 	cout << "I am being printed on processor " << rank << " out of " << size << " total processors" << endl;
 	MPI_Finalize();
-	
+	cout << "Outside MPI Env 2 and rank: unknown?" << endl;
 	return 0;
 }
