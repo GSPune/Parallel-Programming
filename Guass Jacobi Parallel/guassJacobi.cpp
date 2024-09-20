778ba95b6dea19119c44a224d7f20dae1ce6ed56
Stephen George <stephengeorgepune@gmail.com>
2024-09-21 00:36:03 +0530

    Corrected Version (Size % p == 0)

--- Guass Jacobi Parallel/guassJacobi.cpp
+++ Guass Jacobi Parallel/guassJacobi.cpp
@@ -13,7 +13,7 @@ int main(int argc,char** argv)
 	// fin.close();
 	int rank,p;
 	int A[n][n],b[n];
-	double x[n];
+	double x[n]; // decalred on each process but initialized on rank 0
 
     MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&p);
@@ -33,7 +33,6 @@ int main(int argc,char** argv)
 			fin >> b[k];x[k] = 0;
 		}
 		
-		
 		for(int i = 0 ; i < n ; ++i){
 			for (int j = 0 ; j < n; ++j){
 			    cout << A[i][j] << " ";
@@ -48,11 +47,11 @@ int main(int argc,char** argv)
     int rem = n % p, k = n/p;
     int sendCountRows = k*n;
 	int recvbuf[sendCountRows];
-	//send scR rows OF A to each process 
-	//Distributing rows of A
+	//send {k*n} rows OF A to each process 
+	//Distributing rows of A using MPI_Scatter 
 	MPI_Scatter(&A,sendCountRows,MPI_INT,recvbuf,sendCountRows,MPI_INT,0,MPI_COMM_WORLD);
 	
-	//Now to distribute elements of b
+	//Now to distribute {k} elements of b to each process
 	int sendC = k;
 	int recv[sendC];
 	MPI_Scatter(&b,sendC,MPI_INT,recv,sendC,MPI_INT,0,MPI_COMM_WORLD);
@@ -62,28 +61,31 @@ int main(int argc,char** argv)
  	//	cout << recv[i] << " ";
  	//}
  	//cout << endl;
+
+	//Broadcasting X_old = {0,0,0,0} to all processes from source '0'
  	MPI_Bcast(&x,n,MPI_DOUBLE,0,MPI_COMM_WORLD);
- 	for (int it = 0; it < 2; it++){
- 		double xn[k];
+	// cout << "Test :: " << endl;
+ 	for (int it = 0; it < 50; it++){
+ 		double x_new[k];
  		for(int i = 0; i < k ; ++i){
  			// cout << recv[i] << endl;
- 			xn[i] = recv[i];
+ 			x_new[i] = recv[i];
  			for (int j = 0; j < n; ++j){
- 				if((i*n+rank*k+i) != j){
- 					xn[i] -= (recvbuf[j+n*i] * x[j]);
+ 				if((i*n+rank*k+i)%n != j){
+					// cout << recvbuf[j+n*i+rank*k] << endl;
+ 					x_new[i] -= (recvbuf[j+n*i] * x[j]);
  				}
  			}
- 			xn[i] /= (double)recvbuf[i*n+rank*k+i];
+ 			x_new[i] /= (double)recvbuf[i*n+rank*k+i];
  			//cout << x[i] << " " << endl;
  			//cout << xNGathered[i] << " ";
  		}
  		
- 		//Gather the Xnew s from the other processors
+ 		//Gather the X_new s from the other processors
  		double xNGathered[n];
-		MPI_Allgather(&xn,k,MPI_DOUBLE,xNGathered,k,MPI_DOUBLE,MPI_COMM_WORLD);
+		MPI_Allgather(&x_new,k,MPI_DOUBLE,xNGathered,k,MPI_DOUBLE,MPI_COMM_WORLD);
 		
-		
-	 	
+		//update the old x
  		for (int p = 0; p < n; p++){
  			x[p] = xNGathered[p];
  		}
