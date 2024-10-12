1945012173e516363b6dc75db3eb128dcbe69119
Stephen George <stephengeorgepune@gmail.com>
2024-10-12 18:27:27 +0530

    Corrected GE scaling process

--- parallelGE.cpp
+++ parallelGE.cpp
@@ -5,16 +5,20 @@ using namespace std;
 void printMatrix(double A[][5], int n){
 	for(int i = 0; i < n; ++i){
 		for(int j = 0; j < n + 1; ++j){
-			cout << A[i][j] << " ,";
+			cout << A[i][j] << "    ";
 		}
 		cout << endl;
 	}
 	cout << endl;
 }
 
-void Op(double (&B)[5],double (&t)[5],int size, int pivot){
+void Op(double *B,double *t,int size, int pivot){
+	//GE requires a constant uniform scaling across the pivot row during row transformations!
+	//The Gaussian elimination process requires proportional operations across the entire row to maintain the integrity of the system.
+	double factor = B[pivot]/t[pivot];
 	for(int i = 0; i < size; ++i){
-		B[i] -= (double)(B[i]*t[i])/t[pivot];
+		//B[i] -= (double)(B[i]*t[i])/t[pivot];
+		 B[i] -= factor * t[i];
 	}
 }
 
@@ -25,7 +29,7 @@ int main(int argc,char** argv){
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	int n = 4;
 	double A[4][5];
-	double B[5],temp[5];
+	double B[5],temp[5]; // for each processor
 	double reducedA[4][5];
 	if (rank == 0){
 		double copyOfA[4][5] = {{2,3,-1,1,8},{3,2,1,-2,3},{1,1,-2,3,1},{4,0,-1,3,7}};
@@ -45,6 +49,7 @@ int main(int argc,char** argv){
 			Op(B,temp,5,k-1);
 		}
 		MPI_Barrier(MPI_COMM_WORLD);
+		
 		//MPI_Barrier blocks all MPI processes in the given communicator until they all call this routine.
 		//copy the pivot row for next iteration
 		if (rank == k && rank != n-1) memcpy(&temp,&B,sizeof(B));
@@ -52,7 +57,19 @@ int main(int argc,char** argv){
 	MPI_Gather(&B,5,MPI_DOUBLE,&reducedA[rank],5,MPI_DOUBLE,0,MPI_COMM_WORLD);
 	if (rank==0){
 		printMatrix(reducedA,4);
+		//Backsubstitution *
 	}
 	MPI_Finalize();
 	return 0;
 }
+
+/*---------------------Debug stuff----------------------------
+if (k == 2){
+			cout << k << " Set of OPs::" << endl;
+			cout << "Rank " << rank << "::  ";
+			for (int t = 0; t < n+1; ++t){
+				cout << B[t] << " ";
+			}
+			cout << endl;
+		}
+*/
